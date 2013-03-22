10f10134746f63f19839cd217b1bfc60e4499b96
Nathan Henderson <nathos@nathos.com>
2013-03-22 19:04:44 -0400

    added loading spinner to the twitter sidebar widget

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
 
- // fit video embeds to container width
- $(".main-column").fitVids();
+  // fit video embeds to container width
+  $(".main-column").fitVids();
+
+  // Loading spinner for the Twitter widget
+  $("#twitter-loading-spinner").spin();
 
  });
